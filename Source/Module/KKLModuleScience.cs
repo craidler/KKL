16975b2c90522f734bac3de4cd4e44c0be72f3ad
craidler <chris@raidler.ch>
2018-08-14 11:25:10 +0200

    KKLModuleScience

---
+++ Source/Module/KKLModuleScience.cs
@@ -0,0 +1,16 @@
+﻿namespace KKL
+{
+    // ReSharper disable once InconsistentNaming
+    public class KKLModuleScience : PartModule
+    {
+        // ReSharper disable once UnusedMember.Global
+        [KSPEvent(guiActive = false, guiName = "Run Experiments")]
+        public void Run()
+        {
+            foreach (var m in vessel.FindPartModulesImplementing<ModuleScienceExperiment>())
+            {
+                if (m.IsRerunnable()) m.Events[m.experimentActionName].Invoke();
+            }
+        }
+    }
+}
